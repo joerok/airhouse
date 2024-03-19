f27d992f8b09eafe8e22410e1588877c166b6e37
Josef Rokicki <github@rokj.com>
2024-03-19 16:22:40 -0700

    Update test_match.py
    add tests

--- backend/backend/common/tests/test_match.py
+++ backend/backend/common/tests/test_match.py
@@ -42,3 +42,26 @@ class MatchTestCase(TestCase):
 
     def test_assert_lots_of_backtracking(self):
         self.assertTrue(match(r'a*b*a*b*a*b*a*b*a*b*ab', 'abaabbaaabbbaaaabbbbaaaaabbbbbab'))
+
+    def test_basic_patterns(self):
+        patterns = (
+            (r'a*b', 'ab', True, "basic matching"),
+            (r'a.c', 'abc', True, ". bounded by characters'),
+            (r'a+b', 'aab', True, "1+ matching bounded at the end"),
+            (r'', '', True, 'Empty string and empty pattern'),
+            (r'a*', 'aaaaaaaa', 'Wildcard matches many'),
+            (r'a*b', '', False, 'Empty string with bounded wildcard'),
+            (r'a*', 'b', False, 'Wildcard matches only the coefficient'),
+            (r'a+b', 'a', False, 'Plus bounded does not match unbounded string'),
+            (r'a*b*c', 'ac', True, 'Multiple wildcards matches with bounded string'),
+            (r'a*a', 'a*a', True, 'Consecutive wildcard matches with the same character'),
+            (r'a*b*c*', 'aabcc', True, 'Mutiple wildcards with repetitions'),
+            (r'a*b+c', 'abbc', True, 'Mixing plus and wildcards'),
+            (r'a.+c', 'a1c', True, 'Any character matching'),
+            (r'a*b*c', 'abca', False, 'Wildcards do not leave their bounds'))
+
+        for pattern, string, result, note in patterns:
+            if result == False:
+                self.assertFalse(match(pattern, string), note)
+            else:
+                self.assertTrue(match(pattern, string), note)
