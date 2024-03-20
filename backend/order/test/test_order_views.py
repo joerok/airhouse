a9293ed63177b71adb2d05c487116e1a6730113b
Josef Rokicki <github@rokj.com>
2024-03-20 13:02:34 -0700

    test create fake order

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -4,5 +4,9 @@ from rest_framework.test import APITestCase
 class OrderViewSetTestCase(APITestCase):
     def test_order_view_set_list(self):
         response = self.client.get('/api/orders/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_order_create_fake_order(self):
+        response = self.client.get('/api/orders/?fakeorder')
         raise Exception(response.data)
         self.assertEqual(response.status_code, 200)
