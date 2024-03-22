7b1bed984c3b1052f38f8b5868ea3b98e63b88e3
Josef Rokicki <github@rokj.com>
2024-03-21 19:46:28 -0700

    did I do that

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -15,13 +15,13 @@ class OrderViewSetTestCase(APITestCase):
     def test_order_item_patch(self):
         response = self.client.get('/api/orders/?fake_order')
         order_id = response.json()['uuid']
-        response = self.client.get(f'/api/orders/{order_id}', context_type="application/json")
+        response = self.client.get(f'/api/orders/{order_id}/', context_type="application/json")
         try:
             data = response.json()
         except:
             raise Exception(response)
         raise Exception(data)
         order_item_id = data['order_items'][0]['uuid']
-        raise Exception(self.client.patch(f'/api/order_item/{order_item_id}', {"price": 5000, 'currency': 'EUR'}, content_type='application/json'))
+        raise Exception(self.client.patch(f'/api/order_items/{order_item_id}/', {"price": 5000, 'currency': 'EUR'}, content_type='application/json'))
         raise Exception(data)
         self.assertEqual(items[0]['amount'], '$100.00')
