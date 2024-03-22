a4c8833368690403f20653e7073202559ed6095f
Josef Rokicki <github@rokj.com>
2024-03-22 13:28:09 -0700

    complete the tests

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -12,14 +12,10 @@ class OrderViewSetTestCase(APITestCase):
         data = response.json()
         self.assertEqual(response.status_code, 200)
 
-    def test_order_item_patch(self):
+    def test_order_item_update_amount_changes_price_and_currency(self):
         response = self.client.get('/api/orders/?fake_order')
         order_id = response.json()['uuid']
         response = self.client.get(f'/api/orders/{order_id}/', context_type="application/json")
-        try:
-            data = response.json()
-        except:
-            raise Exception(response)
         order_item_id = data['order_items'][0]['uuid']
         order_item = data['order_items'][0]
         order_item.update(
@@ -29,6 +25,28 @@ class OrderViewSetTestCase(APITestCase):
         del order_item['currency']
         response = self.client.put(f'/api/order_items/{order_item_id}/', order_item, format='json')
         self.assertTrue(response.status_code, 201)
+        response = self.client.put(f'/api/order_items/{order_item_id}/', order_item, format='json')
+        order_item = response.json()
+        self.assertEqual(order_item['amount'], '€6000.00')
+        self.assertEqual(order_item['price'], 6000.00)
+        self.assertEqual(order_item['currency'], '€')
+
+
+    def test_order_item_update_price_and_currency_changes_amount(self):
+        response = self.client.get('/api/orders/?fake_order')
+        order_id = response.json()['uuid']
         response = self.client.get(f'/api/orders/{order_id}/', context_type="application/json")
-        raise Exception(response.json())
-        self.assertEqual(items[0]['amount'], '$100.00')
+        order_item_id = data['order_items'][0]['uuid']
+        order_item = data['order_items'][0]
+        order_item.update(
+            currency='€',
+            price='7000'
+        )
+        del order_item['amount']
+        response = self.client.put(f'/api/order_items/{order_item_id}/', order_item, format='json')
+        self.assertTrue(response.status_code, 201)
+        response = self.client.put(f'/api/order_items/{order_item_id}/', order_item, format='json')
+        order_item = response.json()
+        self.assertEqual(order_item['amount'], '€7000.00')
+        self.assertEqual(order_item['price'], 7000.00)
+        self.assertEqual(order_item['currency'], '€')
