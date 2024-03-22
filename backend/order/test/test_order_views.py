26949535880531a165ffedb1559ba69c41bebcb4
Josef Rokicki <github@rokj.com>
2024-03-22 12:24:32 -0700

    change things up

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -13,28 +13,21 @@ class OrderViewSetTestCase(APITestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_order_item_patch(self):
-        from contextlib import redirect_stderr
-        import io
-
-        output = io.StringIO()
-        with redirect_stderr(output) as out:
-            try:
-                response = self.client.get('/api/orders/?fake_order')
-                order_id = response.json()['uuid']
-                response = self.client.get(f'/api/orders/{order_id}/', context_type="application/json")
-                try:
-                    data = response.json()
-                except:
-                    raise Exception(response)
-                order_item_id = data['order_items'][0]['uuid']
-                order_item = data['order_items'][0]
-                order_item.update(
-                    price=5000,
-                    currency='EUR'
-                )
-                raise Exception(self.client.put(f'/api/order_items/{order_item_id}/', order_item, content_type='application/json'))
-                response = self.client.get(f'/api/orders/{order_id}/', context_type="application/json")
-                raise Exception(response.json())
-                self.assertEqual(items[0]['amount'], '$100.00')
-            except:
-                raise self.assertFalse(True, out.getvalue())
+        response = self.client.get('/api/orders/?fake_order')
+        order_id = response.json()['uuid']
+        response = self.client.get(f'/api/orders/{order_id}/', context_type="application/json")
+        try:
+            data = response.json()
+        except:
+            raise Exception(response)
+        order_item_id = data['order_items'][0]['uuid']
+        order_item = data['order_items'][0]
+        order_item.update(
+            price=5000,
+            currency='EUR'
+        )
+        response = self.client.put(f'/api/order_items/{order_item_id}/', order_item, content_type='application/json')
+        raise Exception(response)
+        response = self.client.get(f'/api/orders/{order_id}/', context_type="application/json")
+        raise Exception(response.json())
+        self.assertEqual(items[0]['amount'], '$100.00')
