27a3b43760a4e136a1a182e8f7b57f3c188b3770
Josef Rokicki <github@rokj.com>
2024-03-21 19:35:20 -0700

    debugging

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -15,6 +15,7 @@ class OrderViewSetTestCase(APITestCase):
     def test_order_item_patch(self):
         response = self.client.get('/api/orders/')
         data = response.json()
+        raise Exception(data)
         order_item_id = data['order_items'][0]['uuid']
         raise Exception(self.client.patch(f'/api/order_item/{order_item_id}', {"price": 5000, 'currency': 'EUR'}))
         raise Exception(data)
