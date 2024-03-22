133b13097a8bdfc5766f72b1b5e8f400404cb3bb
Josef Rokicki <github@rokj.com>
2024-03-22 15:37:19 -0700

    try this

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -16,8 +16,6 @@ class OrderViewSetTestCase(APITestCase):
     def test_order_total_price(self):
         fake_order = OrderFactory()
         serializer = OrderSerializer(instance=fake_order)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
         self.assertTrue(serializer.total_price > 0)
 
     def test_order_quantity_ordered(self):
