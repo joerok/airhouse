8406d735bf2452b17f27db11294da3336d011414
Josef Rokicki <github@rokj.com>
2024-03-21 19:31:27 -0700

    create url for order items

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -6,4 +6,5 @@ from order.views import OrderViewSet, OrderItemViewSet
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='order')
 router.register(r'orders/<int:order_id>/items', OrderItemViewSet, basename='order_item')
+router.register(r'order_items', OrderItemViewSet, basename='order_item')
 urlpatterns = router.urls
