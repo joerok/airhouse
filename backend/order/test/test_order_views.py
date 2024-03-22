fca3376cd72ea734fa1a3c877e2a07c506ddfd43
Josef Rokicki <github@rokj.com>
2024-03-22 15:47:27 -0700

    use a float

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -17,7 +17,7 @@ class OrderViewSetTestCase(APITestCase):
     def test_order_total_price(self):
         fake_order = OrderFactory()
         serializer = OrderSerializer(instance=fake_order)
-        self.assertTrue(serializer.data['total_order_price'] > 0)
+        self.assertTrue(float(serializer.data['total_order_price']) > 0)
 
     def test_order_quantity_ordered(self):
         fake_order = OrderFactory()
