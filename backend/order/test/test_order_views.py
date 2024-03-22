293143383997d7ba369c4e143dd4711dbc98ec2a
Josef Rokicki <github@rokj.com>
2024-03-22 15:31:27 -0700

    fix

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -16,11 +16,13 @@ class OrderViewSetTestCase(APITestCase):
     def test_order_total_price(self):
         fake_order = OrderFactory()
         serializer = OrderSerializer(instance=fake_order)
+        serializer.save()
         self.assertTrue(serializer.total_price > 0)
 
     def test_order_quantity_ordered(self):
         fake_order = OrderFactory()
         serializer = OrderSerializer(instance=fake_order)
+        serializer.save()
         self.assertTrue(serializer.quantity_ordered > 0)
 
     def test_order_item_update_amount_changes_price_and_currency(self):
