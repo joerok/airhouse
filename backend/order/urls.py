bb05c27d5be25aed51a476d5624930fa5f876eeb
Josef Rokicki <github@rokj.com>
2024-03-21 19:33:39 -0700

    basename has to be unique

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -5,6 +5,6 @@ from order.views import OrderViewSet, OrderItemViewSet
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='order')
-router.register(r'orders/<int:order_id>/items', OrderItemViewSet, basename='order_item')
+router.register(r'orders/<int:order_id>/items', OrderItemViewSet, basename='order_order_item')
 router.register(r'order_items', OrderItemViewSet, basename='order_item')
 urlpatterns = router.urls
