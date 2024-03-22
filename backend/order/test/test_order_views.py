87d68c417606806e2bd08ffa63567d453d454572
Josef Rokicki <github@rokj.com>
2024-03-22 15:00:28 -0700

    simplify test

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -19,28 +19,20 @@ class OrderViewSetTestCase(APITestCase):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         self.assertEqual(serializer.data['amount'], '€6000.12')
-        self.assertEqual(serializer.data['price'], 6000.12)
+        self.assertEqual(serializer.data['price'], '6000.12')
         self.assertEqual(serializer.data['currency'], 'EUR')
 
     def test_order_item_update_price_and_currency_changes_amount(self):
-        response = self.client.get('/api/orders/?fake_order')
-        order_id = response.json()['uuid']
-        response = self.client.get(f'/api/orders/{order_id}/', context_type="application/json")
-        data = response.json()
-        order_item_id = data['order_items'][0]['uuid']
-        order_item = data['order_items'][0]
-        order_item.update(
-            currency='EUR',
-            price='7000'
-        )
-        del order_item['amount']
-        response = self.client.put(f'/api/order_items/{order_item_id}/', order_item, format='json')
-        self.assertTrue(response.status_code, 201)
-        response = self.client.put(f'/api/order_items/{order_item_id}/', order_item, format='json')
-        order_item = response.json()
-        self.assertEqual(order_item['amount'], '€7000')
-        self.assertEqual(order_item['price'], 7000.00)
-        self.assertEqual(order_item['currency'], 'EUR')
+        fake_order = OrderFactory()
+        serializer = OrderItemSerializer(
+            instance=fake_order.order_items.first(), 
+            data={'currency':'EUR', 'price':'7000.98'},
+            partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        self.assertEqual(serializer.data['amount'], '€7000.98')
+        self.assertEqual(serializer.data['price'], '7000.12')
+        self.assertEqual(serializer.data['currency'], 'EUR')
 
     def test_order_bad_number(self):
         fake_order = OrderFactory()
