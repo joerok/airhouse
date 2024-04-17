d50320485e13f25acbebde02ef87e171c5cfe9a3
Josef Rokicki <github@rokj.com>
2024-04-17 11:07:31 -0700

    again

--- backend/order/test/test_order_views.py
+++ backend/order/test/test_order_views.py
@@ -15,7 +15,7 @@ class OrderViewSetTestCase(APITestCase):
     def tearDown(self):
         from django.conf import settings
         from django.db import connection, reset_queries
-        raise Exception(list(map(lambda x: x[:50], connection.queries)))
+        raise Exception(list(map(lambda x: str(x)[:50], connection.queries['sql'])))
 
         settings.DEBUG = False    
 
