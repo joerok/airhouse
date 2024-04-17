349ea6dbbb663c3aabc0d5c4e66ddf55bbbb592f
Josef Rokicki <github@rokj.com>
2024-04-17 10:54:08 -0700

    strange place to put coalesce

--- backend/order/query.py
+++ backend/order/query.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Coalesce
+from django.db.models.functions import Coalesce
 
 class OrderQueryManager(models.Manager):
     def get_queryset(self):
